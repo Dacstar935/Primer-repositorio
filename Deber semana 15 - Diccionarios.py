28d04a259d383eb783088ea7b5b2bea12e8152ee
Dacstar935 <da.cambim@uea.edu.ec>
2025-03-29 14:51:25 -0500

    Deber de la semana 15 de diccionarios

---
+++ Deber semana 15 - Diccionarios.py
@@ -0,0 +1,13 @@
+diccionario = {"Nombre":"Danny", "Edad": "18", "Ciudad": "Quito", "Profesion": "Estudiante"}
+print (diccionario)
+#Modificar Ciudad y profesion
+diccionario["Ciudad"]= "Guayaquil"
+print (diccionario)
+diccionario["Profesion"]= "Futbolista"
+print (diccionario)
+#Agregar clave
+diccionario["Numero"]= "0123456789"
+print (diccionario)
+#Eliminar clave
+del diccionario ["Edad"]
+print (diccionario)
